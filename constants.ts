64b91ffcca2a2726cfa8592a149a822c9b692256
George Jose <george.jose.a@gmail.com>
2020-01-11 14:38:03 -0500

    Stuff

---
+++ constants.ts
@@ -0,0 +1,3 @@
+export const PLAID_ENV = 'development'
+export const PLAID_PRODUCTS = ['auth', 'transactions', 'balances'].join(',')
+export const PLAID_COUNTRY_CODES = ['US', 'CA', 'GB', 'FR', 'ES']
