b368b9916b396e56910a1aa3c584196dad72a4ba
George Jose <george.jose.a@gmail.com>
2020-01-12 19:41:26 -0500

    Clean up some stuff

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const PLAID_ENV = 'development'
-export const PLAID_PRODUCTS = ['auth', 'transactions'].join(',')
+export const PLAID_PRODUCTS = ['auth', 'transactions', 'balance'].join(',')
 export const PLAID_COUNTRY_CODES = ['US', 'CA', 'GB', 'FR', 'ES'].join(',')
